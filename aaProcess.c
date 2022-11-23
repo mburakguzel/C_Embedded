0cbf75c53c389984408857b459cbdc4ac58bde8d
Burak Guzel <80826790+mburakguzel@users.noreply.github.com>
2022-11-23 19:42:55 +0100

    Add files via upload

---
+++ aaProcess.c
@@ -0,0 +1,28 @@
+// Exercise yazanlardaki exerciseleri notlari bitirince yap!
+// InstallIDE done
+// Print_Output done 
+// Data_Types.c done!
+// StorageClass done!
+// Functions exercise done!
+// Typecasting done!
+// UsePrintfOnMCU Line done!
+// AnalyzeEmbeddedCCode done!
+// Settings for CubeIDE done!
+// Scanf done!
+// Pointers done!
+// Stdint done!
+// Operators bitwise operators exercise section 19 done!
+// IF exercise done!
+// Switch exercise done!
+// ReadWritetoPin exercise section 18 done! Really important!!!!
+// While Exercise video 130 done!
+// For Exercise done!
+// Optimization and Volatile done! Volatile ve consta biraz kafa yormak lazim!
+// Structures exercise section 25 done! Kodlari mutlaka calistir! 
+// Bitfields done! Bitwise -2 de union.c ye referans vermis onu ekle! Keypad exampleina bakabilirsin! 
+// Unions Done!
+// Arrays Done! Kodu dene!
+// Strings Done! Exercise
+// Preprocessor Done! Exercise Led Toggle with macros ornegine bak!
+
+
